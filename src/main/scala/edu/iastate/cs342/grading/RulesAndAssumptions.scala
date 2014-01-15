3dd40e39872c130efb54161524fb61b320745022
Lorand Szakacs <lorand.szakacs@gmail.com>
2014-01-15 13:03:03 -0600

    Added implicit constructor for a student object. Added assummptions object.
    The object RulesAndAssumptions contains function that rely heavily on convention.
    This is a temporary measure to aggregate all computation that would otherwise rely on
    implicit assumptions. When a method is devised of distributing these functions to their
    appropriate context yet make it easy for anyone who reads the code to immediately know
    what the major assumptions of the program are then this class needs to be factored out asap.
    
    Note: this is something that I have never tried before. I am curious how it will turn out.

---
+++ src/main/scala/edu/iastate/cs342/grading/RulesAndAssumptions.scala
@@ -0,0 +1,28 @@
+package edu.iastate.cs342.grading
+
+import edu.iastate.cs342.grading.constants.Constants
+import edu.iastate.cs342.grading.util.IO
+
+/**
+ * @author lorand
+ * 	This object contains functions that handle computation that is heavily dependent on
+ *  convention.
+ *
+ *  This is a temporary class that will help us explicitly state otherwise implicit assumptions.
+ *  Therefore it will be an amalgam of computation from different independent modules.
+ *
+ *  This class should be slowly factored out until it disappears entirely.
+ */
+object RulesAndAssumptions {
+
+  /**
+   * Currently, the format of a repository URL is the following:
+   * https://github.com/ComS342-ISU/hw-answers-netID
+   */
+  def createRepoURLFromStudentID(netID: String) = "https://github.com/ComS342-ISU/hw-answers-" + netID
+
+  /**
+   * Repositories will be saved to a subfolder of the folder pointed to in the application.conf file
+   */
+  def createRepoFileSystemLocationFromStudentID(netID: String) = IO.concatPath(Constants.ConfigValues.PathWhereToDownload, netID)
+}
--- src/main/scala/edu/iastate/cs342/grading/Student.scala
+++ src/main/scala/edu/iastate/cs342/grading/Student.scala
@@ -3,10 +3,10 @@ package edu.iastate.cs342.grading
 import edu.iastate.cs342.grading.constants.Constants
 import edu.iastate.cs342.grading.util.IO
 
-case class Student(val firstName: String, val lastName: String, val email: String, val repoURL: String) extends Equals {
+case class Student private (val firstName: String, val lastName: String, val netID: String, val repoURL: String) extends Equals {
   val fullName = firstName + " " + lastName
 
-  lazy val filePath = IO.concatPath(Constants.ConfigValues.PathWhereToDownload, lastName)
+  lazy val repoFolderPath = RulesAndAssumptions.createRepoFileSystemLocationFromStudentID(netID)
 
   def canEqual(other: Any) = {
     other.isInstanceOf[edu.iastate.cs342.grading.Student]
@@ -14,15 +14,34 @@ case class Student(val firstName: String, val lastName: String, val email: Strin
 
   override def equals(other: Any) = {
     other match {
-      case that: edu.iastate.cs342.grading.Student => that.canEqual(Student.this) && firstName == that.firstName && lastName == that.lastName && email == that.email
+      case that: edu.iastate.cs342.grading.Student => that.canEqual(Student.this) && firstName == that.firstName && lastName == that.lastName && netID == that.netID
       case _ => false
     }
   }
 
   override def hashCode() = {
     val prime = 41
-    prime * (prime * (prime + firstName.hashCode) + lastName.hashCode) + email.hashCode
+    prime * (prime * (prime + firstName.hashCode) + lastName.hashCode) + netID.hashCode
   }
 
-  override val toString = fullName + " " + email
+  override lazy val toString = fullName + " " + netID
 }
+
+object Student {
+  /**
+   * @param lineFromFile this "constructor" expects a string formatted like a line from the students.txt file
+   * The only supported format so far is:
+   * firstName,lastName,netID
+   *
+   */
+  def apply(lineFromFile: String) = {
+    val values = lineFromFile.split(",")
+    assert(values.length == 3, "Read an Invalid line from the students file. Please ensure that the format is: firstName,lastName,netID")
+    val firstName = values(0)
+    val lastName = values(1)
+    val netID = values(2)
+    val repoURL = RulesAndAssumptions.createRepoURLFromStudentID(netID)
+    new Student(firstName, lastName, netID, repoURL)
+  }
+}
+
