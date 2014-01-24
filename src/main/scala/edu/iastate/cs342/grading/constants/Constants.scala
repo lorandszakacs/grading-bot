6d527542c18c70e21aa91979244437675fa7efcc
Lorand Szakacs <lorand.szakacs@gmail.com>
2014-01-23 23:42:22 -0600

    Added racket path to application.conf

--- src/main/scala/edu/iastate/cs342/grading/constants/Constants.scala
+++ src/main/scala/edu/iastate/cs342/grading/constants/Constants.scala
@@ -6,9 +6,11 @@ object Constants {
     private object PropertyKeys {
       val PathWhereToDownload = "grading.path-where-to-download"
       val PathToGit = "grading.path-to-git"
+      val PathToRacket = "grading.path-to-racket"
     }
     private val conf = ConfigFactory.load()
     val PathWhereToDownload = conf.getString(PropertyKeys.PathWhereToDownload)
-    val PathToGit = conf.getString(PropertyKeys.PathToGit) + "git"
+    val PathToGit = conf.getString(PropertyKeys.PathToGit)
+    val PathToRacket = conf.getString(PropertyKeys.PathToRacket)
   }
 }
